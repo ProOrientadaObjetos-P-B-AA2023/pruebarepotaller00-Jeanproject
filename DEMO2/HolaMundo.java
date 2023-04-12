b150db17ef76d337c1ad701bd0ba014d86d58b0e
Jeanproject <116390051+Jeanproject@users.noreply.github.com>
2023-04-12 17:02:20 -0500

    Add files via upload

---
+++ DEMO2/HolaMundo.java
@@ -0,0 +1,7 @@
+package DEMO2;
+
+public class HolaMundo {
+    public static void main(String[] args) {
+            System.out.println("Loja, una ciudad llena de arte y naturaleza con 400322");
+    }
+}
